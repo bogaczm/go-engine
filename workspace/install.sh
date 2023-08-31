284fb8e6ae623036f736eeec469962f10f7a48f7
Michał Bogacz <61432464+bogaczm@users.noreply.github.com>
2023-08-31 21:11:29 +0200

    Update install.sh

--- workspace/install.sh
+++ workspace/install.sh
@@ -5,8 +5,7 @@ echo "Creating virtual environment..."
 python3 -m venv .venv
 source .venv/bin/activate
 
-pip3 install --trusted-host nexus.ds-platform.ey.net -i https://nexus.ds-platform.ey.net/repository/pypi-group/simple \
- --no-cache-dir --use-deprecated=legacy-resolver -U wheel pip==22.* ipython ipykernel -r docker/datascience/requirements.txt
+pip3 install --no-cache-dir --use-deprecated=legacy-resolver -U wheel pip==22.* ipython ipykernel -r docker/datascience/requirements.txt
 
 ipython kernel install --name $PROJECT_NAME --user
 
